593a38b7a537787c21511ea3bc3fbcad1e941668
matekelemen <kelemen.mate.12@gmail.com>
2023-09-17 20:34:04 +0200

    add inverse transform view

--- python/scripts/transform.py
+++ python/scripts/transform.py
@@ -48,6 +48,7 @@ class InteractiveTransform:
                                                [ 1.0,  1.0]]).transpose((1, 0))
         self.__transformType = transformType
         self.__transform = transformType()
+        self.__inverseTransform = self.__transform.makeInverse()
         self.__updateTransform()
 
         # Render parameters
@@ -61,8 +62,7 @@ class InteractiveTransform:
                            int(2 * self.__gridRadius * self.__gridDensity)),
             numpy.linspace(0 - self.__gridRadius,
                            0 + self.__gridRadius,
-                           int(2 * self.__gridRadius * self.__gridDensity))
-        )
+                           int(2 * self.__gridRadius * self.__gridDensity)))
 
         self.__colors: numpy.ndarray
         if valueSource:
@@ -108,19 +108,21 @@ class InteractiveTransform:
         # Transformed grid
         self.__transformedXGrid = numpy.ndarray(self.__xGrid.shape)
         self.__transformedYGrid = numpy.ndarray(self.__xGrid.shape)
+        self.__inverseXGrid = numpy.ndarray(self.__xGrid.shape)
+        self.__inverseYGrid = numpy.ndarray(self.__xGrid.shape)
         self.__updateGrid()
 
         # Gridlines in transformed space
         self.__transformedGrid = numpy.ndarray((2*len(self.__gridCoordinates), 2, self.__gridDensity))
+        self.__inverseGrid = numpy.ndarray((2*len(self.__gridCoordinates), 2, self.__gridDensity))
         self.__updateGridLines()
 
         # Static graphics
-        self.__figure, (self.__localAxes, self.__transformedAxes) = pyplot.subplots(1, 2)
+        self.__figure, (self.__localAxes, self.__transformedAxes, self.__inverseAxes) = pyplot.subplots(1, 3)
         self.__localColorMesh = self.__localAxes.pcolorfast(
             self.__xGrid,
             self.__yGrid,
-            self.__colors
-        )
+            self.__colors)
 
         self.__localGridLines = []
         for i in range(0, self.__localGrid.shape[0]):
@@ -140,8 +142,11 @@ class InteractiveTransform:
         self.__transformedColorMesh = self.__transformedAxes.pcolorfast(
             self.__transformedXGrid,
             self.__transformedYGrid,
-            self.__colors
-        )
+            self.__colors)
+        self.__inverseColorMesh = self.__inverseAxes.pcolorfast(
+            self.__inverseXGrid,
+            self.__inverseYGrid,
+            self.__colors)
 
         self.__transformedGridLines = []
         for i in range(0, self.__transformedGrid.shape[0]):
@@ -149,8 +154,14 @@ class InteractiveTransform:
                 self.__transformedGrid[i,0,:],
                 self.__transformedGrid[i,1,:],
                 color = [0.5, 0.5, 0.5, 1.0],
-                linestyle = "-."
-            ))
+                linestyle = "-."))
+        self.__inverseGridLines = []
+        for i in range(0, self.__inverseGrid.shape[0]):
+            self.__inverseGridLines.append(self.__inverseAxes.plot(
+                self.__inverseGrid[i,0,:],
+                self.__inverseGrid[i,1,:],
+                color = [0.5, 0.5, 0.5, 1.0],
+                linestyle = "-."))
 
         # Interactive graphics
         controlPoints = self.__getControlPoints()
@@ -161,10 +172,13 @@ class InteractiveTransform:
             zorder = 2
         )
 
-        self.__transformedAxes.set_xlim(2.5 * numpy.array([-self.__gridRadius, self.__gridRadius]))
-        self.__transformedAxes.set_ylim(2.5 * numpy.array([-self.__gridRadius, self.__gridRadius]))
+        for axes in (self.__transformedAxes, self.__inverseAxes):
+            axes.set_xlim(2.5 * numpy.array([-self.__gridRadius, self.__gridRadius]))
+            axes.set_ylim(2.5 * numpy.array([-self.__gridRadius, self.__gridRadius]))
+            axes.set_aspect("equal", "box")
+
         self.__transformedAxes.set_title("Transformed Space")
-        self.__transformedAxes.set_aspect("equal", "box")
+        self.__inverseAxes.set_title("Inverse Space")
 
         # Register callbacks
         canvas = self.__figure.canvas
@@ -185,12 +199,22 @@ class InteractiveTransform:
             self.__transformedXGrid,
             self.__transformedYGrid,
             self.__colors,
-            zorder = 0
-        )
+            zorder = 0)
+
+        self.__inverseColorMesh.remove() # <== I just can't believe the mesh is immutable @todo
+        self.__inverseColorMesh = self.__inverseAxes.pcolorfast(
+            self.__inverseXGrid,
+            self.__inverseYGrid,
+            self.__colors,
+            zorder = 0)
 
         for i in range(0, self.__transformedGrid.shape[0]):
             self.__transformedGridLines[i][0].set_xdata(self.__transformedGrid[i,0,:])
             self.__transformedGridLines[i][0].set_ydata(self.__transformedGrid[i,1,:])
+
+        for i in range(0, self.__inverseGrid.shape[0]):
+            self.__inverseGridLines[i][0].set_xdata(self.__inverseGrid[i,0,:])
+            self.__inverseGridLines[i][0].set_ydata(self.__inverseGrid[i,1,:])
 
         # Interactive graphics
         self.__updateTransformedBasis()
@@ -221,6 +245,7 @@ class InteractiveTransform:
         """ @brief Update the transfromation to project the transformed basis."""
         controlPoints = self.__getControlPoints()
         self.__transform = self.__transformType(controlPoints)
+        self.__inverseTransform = self.__transform.makeInverse()
 
 
     def __updateGrid(self) -> None:
@@ -232,6 +257,11 @@ class InteractiveTransform:
                 self.__transformedXGrid[i_row, i_column] = transformedPoint[0]
                 self.__transformedYGrid[i_row, i_column] = transformedPoint[1]
 
+                inversePoint = self.__inverseTransform.evaluate((self.__xGrid[i_row, i_column],
+                                                                 self.__yGrid[i_row, i_column]))
+                self.__inverseXGrid[i_row, i_column] = inversePoint[0]
+                self.__inverseYGrid[i_row, i_column] = inversePoint[1]
+
 
     def __updateGridLines(self) -> None:
         """ @brief Update gridlines in transformed space."""
@@ -241,6 +271,8 @@ class InteractiveTransform:
                 self.__localGrid[2*i_main+1,:,i_sub] = (subcoordinate, maincoordinate)
                 self.__transformedGrid[2*i_main,:,i_sub]   = self.__transform.evaluate((maincoordinate, subcoordinate))
                 self.__transformedGrid[2*i_main+1,:,i_sub] = self.__transform.evaluate((subcoordinate, maincoordinate))
+                self.__inverseGrid[2*i_main,:,i_sub]   = self.__inverseTransform.evaluate((maincoordinate, subcoordinate))
+                self.__inverseGrid[2*i_main+1,:,i_sub] = self.__inverseTransform.evaluate((subcoordinate, maincoordinate))
 
 
     def __mapBasisIndex(self, basisIndex: int) -> typing.Optional[int]:
@@ -254,6 +286,13 @@ class InteractiveTransform:
                 3 : 0
             }
         elif isinstance(self.__transform, cie.fem.maths.ScaleTranslateTransform2D):
+            map = {
+                0 : 0,
+                1 : None,
+                2 : None,
+                3 : 1
+            }
+        elif isinstance(self.__transform, cie.fem.maths.TranslateScaleTransform2D):
             map = {
                 0 : 0,
                 1 : None,
@@ -316,7 +355,7 @@ if __name__ == "__main__":
                         "--transform-type",
                         type = str,
                         dest = "transformType",
-                        choices = ["scale", "scale-translate", "affine", "projective"],
+                        choices = ["scale", "scale-translate", "translate-scale", "affine", "projective"],
                         default = "projective")
     parser.add_argument("-i",
                         "--image-url",
@@ -334,6 +373,7 @@ if __name__ == "__main__":
         transformType = {
             "scale"             : cie.fem.maths.OrthogonalScaleTransform2D,
             "scale-translate"   : cie.fem.maths.ScaleTranslateTransform2D,
+            "translate-scale"   : cie.fem.maths.TranslateScaleTransform2D,
             "affine"            : cie.fem.maths.AffineTransform2D,
             "projective"        : cie.fem.maths.ProjectiveTransform2D
         }[arguments.transformType],
