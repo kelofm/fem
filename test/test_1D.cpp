7bafca4ddd0fc03492d113c98a1a6b658cc86f9c
matekelemen <kelemen.mate.12@gmail.com>
2024-09-04 11:01:04 +0200

    WIP: update 1D system test

--- test/test_1D.cpp
+++ test/test_1D.cpp
@@ -1,39 +1,44 @@
 // --- Utility Includes ---
 #include "packages/testing/inc/essentials.hpp"
+#include "packages/stl_extension/inc/SymmetricPair.hpp"
+#include "packages/io/inc/MatrixMarket.hpp"
 
 // --- FEM Includes ---
-#include "packages/graph/inc/Partition.hpp"
-#include "packages/graph/inc/PartitionManager.hpp"
 #include "packages/graph/inc/Graph.hpp"
 #include "packages/graph/inc/BoundaryID.hpp"
-#include "packages/maths/inc/LambdaExpression.hpp"
-#include "packages/maths/inc/ScaleTranslateTransform.hpp"
-#include "packages/numeric/inc/GaussLegendreQuadrature.hpp"
+#include "packages/graph/inc/connectivity.hpp"
+#include "packages/graph/inc/Assembler.hpp"
 #include "packages/maths/inc/Polynomial.hpp"
 #include "packages/maths/inc/AnsatzSpace.hpp"
+#include "packages/utilities/inc/AttributeContainer.hpp"
 
 // --- STL Includes ---
-#include <ranges>
-#include <iostream>
+#include <ranges> // ranges::iota
 
 
 namespace cie::fem {
 
 
+using Scalar = double;
+using CellID = Size;
+using NodeID = Size;
+using DoFID  = Size;
+
+
 template <class TAnsatzSpace>
 struct CellData
 {
-    StaticArray<Size,2>                                 nodeIndices;
-    StaticArray<Size,2>                                 dofMap;
-    std::shared_ptr<TAnsatzSpace>                       p_ansatzSpace;
-    std::shared_ptr<typename TAnsatzSpace::Derivative>  p_ansatzDerivatives;
+    StaticArray<NodeID,2>                               nodeIndices;
+    DynamicArray<DoFID>                                 dofMap;
+    std::shared_ptr<TAnsatzSpace>                       pAnsatzSpace;
+    std::shared_ptr<typename TAnsatzSpace::Derivative>  pAnsatzDerivatives;
 }; // struct CellData
 
 
 struct VertexData
 {
     double position;
-};
+}; // struct VertexData
 
 
 /** @brief 1D system test.
@@ -55,50 +60,99 @@ struct VertexData
 CIE_TEST_CASE("1D", "[systemTests]")
 {
     CIE_TEST_CASE_INIT("1D")
-
-    using Number = double;
     constexpr unsigned Dimension = 1;
+    constexpr Size nodeCount = 10;
 
     // Construct a linear 1D ansatz space
-    using Basis = maths::Polynomial<Number>;
+    using Basis = maths::Polynomial<Scalar>;
     using Ansatz = maths::AnsatzSpace<Basis,Dimension>;
-    auto p_ansatzSpace = std::make_shared<Ansatz>(Ansatz::AnsatzSet {
+    auto pAnsatzSpace = std::make_shared<Ansatz>(Ansatz::AnsatzSet {
         Basis({ 0.5,  0.5}),
         Basis({ 0.5, -0.5})
     });
-    auto p_ansatzDerivatives = std::make_shared<Ansatz::Derivative>(p_ansatzSpace->makeDerivative());
+    auto pAnsatzDerivatives = std::make_shared<Ansatz::Derivative>(pAnsatzSpace->makeDerivative());
+
+    // Find ansatz functions that coincide on opposite boundaries.
+    // In adjacent elements, these ansatz functions will have to map
+    // to the same DoF in the assembled system.
+    const StaticArray<Scalar,5> samples {-1.0, -0.5, 0.0, 0.5, 1.0};
+    const auto ansatzMap = makeAnsatzMap(*pAnsatzSpace,
+                                         samples,
+                                         utils::Comparison<Scalar>(1e-8, 1e-6));
 
     // Construct mesh
-    // The mesh is modeled by a graph whose vertices are cells, and
-    // edges represent the boundaries between adjacent cells. The graph
+    // The mesh is modeled by an adjacency graph whose vertices are cells,
+    // and edges represent the boundaries between adjacent cells. The graph
     // is not directed, but the BoundaryID of the graph edge is always
     // defined on the edge's source cell.
     using Mesh = Graph<void,BoundaryID>;
     Mesh mesh;
-    for (Size i_element : std::ranges::views::iota(0, 9)) {
-        const Size i_leftNode = i_element;
-        const Size i_rightNode = i_element + 1;
-        mesh.insert(Mesh::Edge(i_element, {i_leftNode, i_rightNode}));
+
+    // Insert cells into the adjacency graph
+    for (Size iCell : std::ranges::views::iota(0ul, nodeCount)) {
+        // Insert the cell into the adjacency graph (mesh) as a vertex
+        mesh.insert(Mesh::Vertex(
+            Mesh::VertexID(iCell),
+            {} // <== edges of the adjacency graph are added automatically during edge insertion
+        ));
     }
 
-    // Set graph properties
-    for (Mesh::Edge& r_boundary : mesh.edges()) {
-        // All boundaries connect cells on the left to cells on the right,
-        // so each edge's direction is in the positive x direction.
-        r_boundary.data() = BoundaryID("+x");
+    AttributeContainer<CellData<Ansatz>> cellAttributes(mesh.vertices().size());
+
+    // Insert shared element boundaries into the adjacency graph as edges
+    for (Size iBoundary : std::ranges::views::iota(0ul, nodeCount - 1)) {
+        const Size iLeftCell = iBoundary;
+        const Size iRightCell = iBoundary + 1;
+        mesh.insert(Mesh::Edge(
+            iBoundary,
+            {iLeftCell, iRightCell},
+            "+x" // <== all bundaries connect cells on the left to cells on the right
+        ));
     }
 
-    // Construct cell attributes
-    AttributeContainer<
-        CellData<Ansatz>
-    > cells;
+    // Define and fill cell attributes
+    std::vector<utils::SymmetricPair<Size>> dofPairs;
+    for (Ref<const Mesh::Edge> rBoundary : mesh.edges()) {
+        //Ref<CellData<Ansatz>> rSourceAttributes = cellAttributes.at<CellData<Ansatz>>(rBoundary.source());
+        //Ref<CellData<Ansatz>> rTargetAttributes = cellAttributes.at<CellData<Ansatz>>(rBoundary.target());
 
-    // Construct node attributes
-    AttributeContainer<
-        Number // <== position
-    > nodes;
+        // Get coincident DoFs
+        dofPairs.clear();
+        ansatzMap.getPairs(rBoundary.data(), std::back_inserter(dofPairs));
 
+        // Assign global DoF IDs to local DoFs. The following scenarios are possible:
+        // - neither cells' DoFs have been assigned global DoF IDs yet. This case is the
+        //   simplest, since they can be issued new ones and we only have to take special
+        //   care of coincident DoFs.
+        // - one of the cells has already been assigned global DoF IDs. This case is also
+        //   straightforward, as coincident DoFs of the unassigned cell can be assigned
+        //   global DoF IDs from the other cell.
+        // - both cells have already been assigned global DoF IDs. This one's tricky,
+        //   because the global IDs of coincident DoFs must match, which might require
+        //   deleting existing DoFs if this condition is not already satisfied.
+    }
 
+    Assembler assembler;
+    assembler.addGraph(mesh,
+                       []([[maybe_unused]] Ref<const Mesh::Vertex> rVertex) {return 2ul;},
+                       [&ansatzMap](Ref<const Mesh::Edge> rEdge, Assembler::DoFPairIterator it) {ansatzMap.getPairs(rEdge.data(), it);});
+
+    // Create empty CSR matrix
+    std::size_t rowCount, columnCount;
+    DynamicArray<std::size_t> rowExtents, columnIndices;
+    DynamicArray<double> nonzeros;
+    assembler.makeCSRMatrix(rowCount, columnCount, rowExtents, columnIndices, nonzeros);
+
+    {
+        std::ofstream file("output.mm");
+        utils::io::MatrixMarket::Output io(file);
+        io(rowCount,
+           columnCount,
+           nonzeros.size(),
+           rowExtents.data(),
+           columnIndices.data(),
+           nonzeros.data());
+    }
 }
 
 
