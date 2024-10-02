35c0bfb83a9842e2e8cdb0af7e51b497cda292b2
matekelemen <kelemen.mate.12@gmail.com>
2024-10-02 20:04:18 +0200

    update numpy types

--- python/modules/fem/utilities/permutation.py
+++ python/modules/fem/utilities/permutation.py
@@ -3,11 +3,11 @@ import itertools
 import numpy as np
 
 # ---------------------------------------------------------
-def binaryPermutation( numberOfBits, dtype=np.int ):
+def binaryPermutation( numberOfBits, dtype=np.int64 ):
     return np.asarray( [list(i) for i in itertools.product([0,1], repeat=numberOfBits)], dtype=dtype )
 
 
-def oneHotArray( numberOfBits, dtype=np.int ):
+def oneHotArray( numberOfBits, dtype=np.int64 ):
     return np.asarray( [ [int(i==j) for i in range(numberOfBits)] for j in range(numberOfBits) ], dtype=dtype )
 
 
